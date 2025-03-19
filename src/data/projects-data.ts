9fc939dc8ef9b0628b2717a358afc9c6865bf8d5
cotanol <salcedomoralesfranciscojose@gmail.com>
2025-03-19 18:21:35 -0500

    Mi Portfolio V2

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -1,23 +1,30 @@
 export const projects = [
   {
     title: "Blog-Fake",
-    description: "A modern e-commerce platform built with React and Node.js",
+    description: "A modern e-commerce platform built with Django and SQLite",
     image: "./projects/blog-fake.webp",
     technologies: ["Python", "Django", "SQLite"],
     link: "https://cotanol.pythonanywhere.com/",
   },
   {
     title: "Ecommerce Coursestore",
-    description: "A collaborative task management application",
+    description: "A modern e-commerce platform built with Next and Firebase",
     image: "./projects/courses-page.webp",
     technologies: ["Next", "Firebase", "Tailwind CSS", "TypeScript"],
     link: "https://1er-proyecto-next-firebase.vercel.app/",
   },
   {
     title: "Ecommerce Bookstore",
-    description: "A modern e-commerce platform built with React and Node.js",
+    description: "A modern e-commerce platform built with React",
     image: "./projects/libreria.webp",
     technologies: ["React", "TypeScript", "Firebase", "Tailwind CSS"],
+    link: "https://proyecto-libreria-react-firebase.vercel.app/",
+  },
+  {
+    title: "Shop Ecommerce",
+    description: "A modern e-commerce platform built with Next and Supabase",
+    image: "./projects/shop-ecommerce.webp",
+    technologies: ["Next", "TypeScript", "Supabase", "Tailwind CSS"],
     link: "https://proyecto-libreria-react-firebase.vercel.app/",
   },
 ];
