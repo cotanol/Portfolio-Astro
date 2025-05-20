4f3be57d48c5f81762accc706d0f052dc091b1a7
cotanol <salcedomoralesfranciscojose@gmail.com>
2025-05-20 14:58:44 -0500

    Projecto CourseStore eliminado

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -1,5 +1,12 @@
 export const projects = [
   {
+    title: "Shop Ecommerce",
+    description: "A modern e-commerce platform built with Next and Supabase",
+    image: "./projects/shop-ecommerce.webp",
+    technologies: ["Next", "TypeScript", "Supabase", "Tailwind CSS"],
+    link: "https://shop-supabase-next.vercel.app/",
+  },
+  {
     title: "Blog-Fake",
     description: "A modern e-commerce platform built with Django and SQLite",
     image: "./projects/blog-fake.webp",
@@ -7,24 +14,10 @@ export const projects = [
     link: "https://cotanol.pythonanywhere.com/",
   },
   {
-    title: "Ecommerce Coursestore",
-    description: "A modern e-commerce platform built with Next and Firebase",
-    image: "./projects/courses-page.webp",
-    technologies: ["Next", "Firebase", "Tailwind CSS", "TypeScript"],
-    link: "https://1er-proyecto-next-firebase.vercel.app/",
-  },
-  {
     title: "Ecommerce Bookstore",
     description: "A modern e-commerce platform built with React",
     image: "./projects/libreria.webp",
     technologies: ["React", "TypeScript", "Firebase", "Tailwind CSS"],
     link: "https://proyecto-libreria-react-firebase.vercel.app/",
-  },
-  {
-    title: "Shop Ecommerce",
-    description: "A modern e-commerce platform built with Next and Supabase",
-    image: "./projects/shop-ecommerce.webp",
-    technologies: ["Next", "TypeScript", "Supabase", "Tailwind CSS"],
-    link: "https://shop-supabase-next.vercel.app/",
   },
 ];
