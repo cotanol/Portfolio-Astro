8459d27cb0058f1562ac7016af7be5037df8298b
cotanol <salcedomoralesfranciscojose@gmail.com>
2026-06-26 10:38:52 -0500

    Logo

--- src/data/skill-data.ts
+++ src/data/skill-data.ts
@@ -1,19 +1,21 @@
 export const skills = [
+  { name: "javascript", color: "text-[#FF5D01]", animation: "bounce" },
+  { name: "typescript", color: "text-[#FF5D01]", animation: "bounce" },
   { name: "react", color: "text-[#61DAFB]", animation: "spin" },
-  { name: "django", color: "text-white", animation: "bounce" },
-  { name: "firebase", color: "text-[#FF5D01]", animation: "bounce" },
-  { name: "mysql", color: "text-white", animation: "bounce" },
   { name: "next", color: "text-white", animation: "spin" },
+
+  { name: "python", color: "text-[#4F94CD]", animation: "bounce" },
+  { name: "fastapi", color: "text-[#FF6B35]", animation: "bounce" },
+  { name: "django", color: "text-white", animation: "bounce" },
+  { name: "nestjs", color: "text-[#3C7CD6]", animation: "bounce" },
+
+  { name: "postgresql", color: "text-[#FF5D01]", animation: "bounce" },
+  { name: "oracle", color: "text-[#FF5D01]", animation: "bounce" },
+  { name: "prisma", color: "text-[#946FDA]", animation: "bounce" },
+  { name: "mysql", color: "text-white", animation: "bounce" },
+
+  { name: "langchain", color: "text-[#FF5D01]", animation: "bounce" },
   { name: "git", color: "text-[#FF6B35]", animation: "spin" },
   { name: "supabase", color: "text-[#3ecf8e]", animation: "spin" },
-  { name: "astro", color: "text-[#FF5D01]", animation: "bounce" },
-
-  { name: "html", color: "text-[#FF6B35]", animation: "bounce" },
-  { name: "css", color: "text-[#3C7CD6]", animation: "bounce" },
-  { name: "javascript", color: "text-[#FF5D01]", animation: "bounce" },
-  { name: "bootstrap", color: "text-[#946FDA]", animation: "bounce" },
-  { name: "typescript", color: "text-[#FF5D01]", animation: "bounce" },
-  { name: "python", color: "text-[#4F94CD]", animation: "bounce" },
-  { name: "java", color: "text-[#FF5D01]", animation: "bounce" },
-  { name: "tailwind", color: "text-[#FF5D01]", animation: "bounce" },
+  { name: "docker", color: "text-[#FF5D01]", animation: "bounce" },
 ];
